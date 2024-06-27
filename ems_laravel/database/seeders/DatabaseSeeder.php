f6a3b0c46dddc12900ffca7a003693b0b33ddc97
Khen <paler.khen.ustp@gmail.com>
2024-06-27 08:55:16 +0800

    removing the duplicated tables in the database and adding seeders for admin and organizer - to run the seeders php artisan migrate:fresh --seed

--- ems_laravel/database/seeders/DatabaseSeeder.php
+++ ems_laravel/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -23,16 +23,47 @@ class DatabaseSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'role_name' => 'participant',
+                'role_description' => 'Event participant role',
+                'permission_type' => 'limited_access',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
                 'role_name' => 'organizer',
                 'role_description' => 'Event organizer role',
                 'permission_type' => 'limited_access',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+        ]);
+
+        DB::table('users')->insert([
             [
-                'role_name' => 'participant',
-                'role_description' => 'Event participant role',
-                'permission_type' => 'read_only',
+                'role_id' => 1,
+                'first_name' => 'admin',
+                'last_name' => 'admin',
+                'gender' => 'Male',
+                'date_of_birth' => '2001-01-01',
+                'email' => 'admin@gmail.com',
+                'user_name' => 'admin',
+                'password' => Hash::make('admin'),
+                'mobile_number' => '09123456789',
+                'country' => 'Philippines',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'role_id' => 3,
+                'first_name' => 'organizer',
+                'last_name' => 'organizer',
+                'gender' => 'Male',
+                'date_of_birth' => '2003-03-03', 
+                'email' => 'organizer@gmail.com',
+                'user_name' => 'organizer',
+                'password' => Hash::make('organizer'),
+                'mobile_number' => '09123456789',
+                'country' => 'Philippines',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
